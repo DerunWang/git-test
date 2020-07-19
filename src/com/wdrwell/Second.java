7832fdc07470ea47b072869d8ed8770c5653c6c9
thejavaer <2939716453@qq.com>
2020-07-19 12:57:39 +0800

    上传Second.java

---
+++ src/com/wdrwell/Second.java
@@ -0,0 +1,9 @@
+package com.wdrwell;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Second");
+        System.out.println("Second");
+        System.out.println("Second");
+    }
+}
