ed6da8104dd664afdebe458abc3968c3816975a8
thejavaer <2939716453@qq.com>
2020-07-19 13:25:00 +0800

    在User中添加date

--- src/com/wdrwell/domain/User.java
+++ src/com/wdrwell/domain/User.java
@@ -3,6 +3,16 @@ package com.wdrwell.domain;
 public class User {
     private String name;
 
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getName() {
         return name;
     }
