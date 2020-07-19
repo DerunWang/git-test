5af6253c545f4582ca41f6221d8f5fe7f8e06db6
thejavaer <2939716453@qq.com>
2020-07-19 12:37:41 +0800

    添加First

---
+++ src/com/wdrwell/First.java
@@ -0,0 +1,9 @@
+package com.wdrwell;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
